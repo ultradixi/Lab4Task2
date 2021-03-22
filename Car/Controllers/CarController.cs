0ddeaa6a362a0bb8b7312904fa76559d883e707b
Diana Oltean <dyana_mysport@yahoo.com>
2021-03-22 15:27:02 +0000

    First commit

---
+++ Car/Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using Car.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Car.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+           Vehicle myCar = new Vehicle() { Make = "Toyota", Model = "Auris", Engine = EngineType.Hybrid, EngineSize = 20 };
+            return View(myCar);
+        }
+    }
+}
---
+++ Car/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car.Models
+{   public enum EngineType { Petrol, Diesel, Hybrid, Electric}
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public EngineType Engine { get; set; }
+        public int EngineSize { get; set; }
+        
+    }
+}
